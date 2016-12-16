ce69613612bfbcf32f54f6e8c4f8c70c83ce1b33
Andy Boyle <andy@sudostream.io>
2016-12-16 09:55:06 +0000

    refactor the flow pieces to be a little clearer

--- src/main/scala/io/sudostream/api_event_horizon/aeh_actor/api/kafka/ProcessApiDefinition.scala
+++ src/main/scala/io/sudostream/api_event_horizon/aeh_actor/api/kafka/ProcessApiDefinition.scala
@@ -1,11 +1,15 @@
 package io.sudostream.api_event_horizon.aeh_actor.api.kafka
 
-import akka.Done
 import akka.actor.ActorSystem
 import akka.event.LoggingAdapter
+import akka.kafka.ConsumerMessage.{Committable, CommittableMessage, CommittableOffset}
+import akka.kafka.ProducerMessage.Message
+import akka.kafka._
+import akka.kafka.scaladsl.Consumer.Control
 import akka.kafka.scaladsl.{Consumer, Producer}
-import akka.kafka.{ConsumerSettings, ProducerMessage, ProducerSettings, Subscriptions}
 import akka.stream.Materializer
+import akka.stream.scaladsl.{Flow, Sink, Source}
+import akka.{Done, NotUsed}
 import io.sudostream.api_event_horizon.messages.GeneratedTestsEvent
 import org.apache.kafka.clients.producer.ProducerRecord
 
@@ -19,28 +23,33 @@ trait ProcessApiDefinition {
   implicit val materializer: Materializer
 
   def kafkaConsumerBootServers: String
-
   def kafkaProducerBootServers: String
-
   def consumerSettings: ConsumerSettings[Array[Byte], GeneratedTestsEvent]
-
   def producerSettings: ProducerSettings[Array[Byte], String]
-
   def logger: LoggingAdapter
 
-  def publishStuffToKafka() : Future[Done] = {
-    val done = Consumer.committableSource(consumerSettings, Subscriptions.topics("generated-test-script"))
-      .map {
-        msg =>
-          println("Test Script = " + msg.record.value())
+  def publishStuffToKafka(): Future[Done] = {
+    val source: Source[CommittableMessage[Array[Byte], GeneratedTestsEvent], Control] =
+      Consumer.committableSource(consumerSettings, Subscriptions.topics("generated-test-script"))
 
-          ProducerMessage.Message(new ProducerRecord[Array[Byte], String](
-            "test-results",
-            msg.record.value + "::: Has been tested"
-          ), msg.committableOffset)
-      }.runWith(Producer.commitableSink(producerSettings))
+    val sink: Sink[Message[Array[Byte], String, Committable], Future[Done]] =
+      Producer.commitableSink(producerSettings)
 
-    done
+    val flow: Flow[
+      CommittableMessage[Array[Byte], GeneratedTestsEvent],
+      Message[Array[Byte], String, CommittableOffset], NotUsed] =
+      Flow[ConsumerMessage.CommittableMessage[Array[Byte], GeneratedTestsEvent]]
+        .map {
+          msg =>
+            println("Test Script = " + msg.record.value())
+
+            ProducerMessage.Message(new ProducerRecord[Array[Byte], String](
+              "test-results",
+              msg.record.value + "::: Has been tested"
+            ), msg.committableOffset)
+        }
+
+    source.via(flow).runWith(sink)
   }
 
 }
