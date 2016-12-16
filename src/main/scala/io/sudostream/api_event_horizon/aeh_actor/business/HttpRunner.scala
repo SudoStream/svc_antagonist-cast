2997fa2dce9c9b3ae968baaad29a50b6e7dc1b9f
Andy Boyle <andy@sudostream.io>
2016-12-16 13:41:12 +0000

    block then return response

---
+++ src/main/scala/io/sudostream/api_event_horizon/aeh_actor/business/HttpRunner.scala
@@ -0,0 +1,14 @@
+package io.sudostream.api_event_horizon.aeh_actor.business
+
+
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
+
+import scala.concurrent.Future
+import scala.util.{Success, Failure}
+
+class HttpRunner {
+
+}
+
+
