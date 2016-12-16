f7b205a7c2e05855fbdbab0c10ebcc528b01cb98
Andy Boyle <andy@sudostream.io>
2016-12-16 13:40:55 +0000

    block then return response

--- src/main/scala/io/sudostream/api_event_horizon/aeh_actor/api/kafka/ProcessApiDefinition.scala
+++ src/main/scala/io/sudostream/api_event_horizon/aeh_actor/api/kafka/ProcessApiDefinition.scala
@@ -1,19 +1,24 @@
 package io.sudostream.api_event_horizon.aeh_actor.api.kafka
 
+import akka.Done
 import akka.actor.ActorSystem
 import akka.event.LoggingAdapter
-import akka.kafka.ConsumerMessage.{Committable, CommittableMessage, CommittableOffset}
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
+import akka.kafka.ConsumerMessage.{Committable, CommittableMessage}
 import akka.kafka.ProducerMessage.Message
 import akka.kafka._
 import akka.kafka.scaladsl.Consumer.Control
 import akka.kafka.scaladsl.{Consumer, Producer}
 import akka.stream.Materializer
 import akka.stream.scaladsl.{Flow, Sink, Source}
-import akka.{Done, NotUsed}
+import io.sudostream.api_event_horizon.aeh_actor.business.HttpRunner
 import io.sudostream.api_event_horizon.messages.GeneratedTestsEvent
 import org.apache.kafka.clients.producer.ProducerRecord
 
-import scala.concurrent.{ExecutionContextExecutor, Future}
+import scala.concurrent.duration._
+import scala.concurrent.{Await, ExecutionContextExecutor, Future}
+import scala.util.{Failure, Success}
 
 trait ProcessApiDefinition {
 
@@ -35,21 +40,35 @@ trait ProcessApiDefinition {
     val sink: Sink[Message[Array[Byte], String, Committable], Future[Done]] =
       Producer.commitableSink(producerSettings)
 
-    val flow: Flow[
-      CommittableMessage[Array[Byte], GeneratedTestsEvent],
-      Message[Array[Byte], String, CommittableOffset], NotUsed] =
+    val flow =
       Flow[ConsumerMessage.CommittableMessage[Array[Byte], GeneratedTestsEvent]]
         .map {
           msg =>
-            println("Test Script = " + msg.record.value())
+            val testScript = msg.record.value()
+            println("Test Script = " + testScript)
 
-            ProducerMessage.Message(new ProducerRecord[Array[Byte], String](
-              "test-results",
-              msg.record.value + "::: Has been tested"
-            ), msg.committableOffset)
+            val testResults = runTestScript(testScript)
+
+            ProducerMessage.Message(
+              new ProducerRecord[Array[Byte], String]("test-results", testResults),
+              msg.committableOffset)
         }
 
     source.via(flow).runWith(sink)
   }
+
+  def runTestScript(testScript: GeneratedTestsEvent): String = {
+    val httpRunner = new HttpRunner
+
+    runTest(TestToRun("helloThere"))
+  }
+
+  def runTest(testToRun: TestToRun): String = {
+    val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri = "http://akka.io"))
+    val res = Await.result(responseFuture, 5 seconds)
+    res.toString()
+  }
+
+  case class TestToRun(testName: String)
 
 }
