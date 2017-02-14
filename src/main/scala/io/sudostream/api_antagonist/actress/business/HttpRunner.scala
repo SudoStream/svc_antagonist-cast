e4835433fb18a8074f965c0029b0d73318c7fb5d
Andy Boyle <andy@sudostream.io>
2017-02-14 15:12:13 +0000

    remove unused imports

--- src/main/scala/io/sudostream/api_antagonist/actress/business/HttpRunner.scala
+++ src/main/scala/io/sudostream/api_antagonist/actress/business/HttpRunner.scala
@@ -1,11 +1,5 @@
 package io.sudostream.api_antagonist.actress.business
 
-import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
-
-import scala.concurrent.Future
-import scala.util.{Success, Failure}
-
 class HttpRunner {
 
 }
